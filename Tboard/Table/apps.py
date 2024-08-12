e1a04baa1e3de5bb2dccde1b7c85a15c45bb4984
JJaehhheee <j01064872275@gmail.com>
2024-08-12 14:11:38 +0900

    project, app setting

--- Tboard/Table/apps.py
+++ Tboard/Table/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ShowtimeConfig(AppConfig):
+class TableConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "showTime"
+    name = "Table"
